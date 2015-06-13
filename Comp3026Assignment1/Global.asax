997ad99c02a619d7bb9d6f68e0a91fd436029c01
Jeff Geear <jmgeear@gmail.com>
2015-06-12 23:04:46 -0400

    created project

---
+++ Comp3026Assignment1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Comp3026Assignment1.MvcApplication" Language="C#" %>
---
+++ Comp3026Assignment1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp3026Assignment1.Startup))]
+namespace Comp3026Assignment1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
