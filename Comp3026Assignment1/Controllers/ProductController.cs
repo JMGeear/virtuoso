d0cb4a8ea248a9e271a1eaeeecd07fdac919449a
AmusableBman <AmusableBman@gmail.com>
2015-06-17 17:31:41 -0400

    Small controller setup

---
+++ Comp3026Assignment1/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comp3026Assignment1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
