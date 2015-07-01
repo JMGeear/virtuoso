f64528bfd8fa5d02f3d056c4caff3980ba557ee1
AmusableBman <AmusableBman@gmail.com>
2015-07-01 19:35:22 -0400

    index, contact, about, footer, nav, adding logo

--- Comp3026Assignment1/Controllers/HomeController.cs
+++ Comp3026Assignment1/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Comp3026Assignment1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
