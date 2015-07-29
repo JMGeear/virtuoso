3a82d6e8f58171595ff08065ad990be1817bacac
AmusableBman <AmusableBman@gmail.com>
2015-07-29 12:15:38 -0400

    adding authorization tags so users cannot edit, create, or delete products

--- Comp3026Assignment1/Controllers/ProductsController.cs
+++ Comp3026Assignment1/Controllers/ProductsController.cs
@@ -37,6 +37,7 @@ namespace Comp3026Assignment1.Controllers
         }
 
         // GET: Products/Create
+        [Authorize]
         public ActionResult Create()
         {
             return View();
@@ -47,6 +48,7 @@ namespace Comp3026Assignment1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<ActionResult> Create([Bind(Include = "ProductID,Name,Price,BrandID,image")] Product product)
         {
             if (ModelState.IsValid)
@@ -60,6 +62,7 @@ namespace Comp3026Assignment1.Controllers
         }
 
         // GET: Products/Edit/5
+        [Authorize]
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
@@ -79,6 +82,7 @@ namespace Comp3026Assignment1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<ActionResult> Edit([Bind(Include = "ProductID,Name,Price,BrandID,image")] Product product)
         {
             if (ModelState.IsValid)
@@ -91,6 +95,7 @@ namespace Comp3026Assignment1.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize]
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
@@ -108,6 +113,7 @@ namespace Comp3026Assignment1.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
             Product product = await db.Products.FindAsync(id);
